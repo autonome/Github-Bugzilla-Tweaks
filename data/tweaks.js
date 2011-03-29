04b7075cc786b523a3d2c3b92409acbd11f10904
Dietrich Ayala <autonome@gmail.com>
2011-03-29 17:28:58 +0700

    Change install link in README, make button label show bug # or 'Attach to new bug'.

--- data/tweaks.js
+++ data/tweaks.js
@@ -1,15 +1,31 @@
 var list = document.getElementsByClassName("js-hard-tabs smalltabs")[0];
 
 if (list) {
-  var titleParts = document.title.split(" ");
-  var bug = titleParts[1].substr(0, 6);
-
+  var bug = getBugNumber(document.title);
+  console.log("BUG #: " + bug);
   var li = document.createElement("li");
-  li.innerHTML = "<a href='#'>Attach to Bug</a>";
-  li.addEventListener("click", send, false);
+  
+  if (bug)
+    li.innerHTML = "<a href='#'>Attach to Bug " + bug + "</a>";
+  else
+    li.innerHTML = "<a href='#'>Attach to a New Bug</a>";
+
+  li.addEventListener("click", function() {
+    send(bug, document.location.toString());
+  }, false);
+
   list.appendChild(li);
 }
 
-function send() {
-  postMessage({bug: bug, url: document.location.toString()});
+function send(bugNumber, pullRequestURL) {
+  postMessage({bug: bugNumber, url: pullRequestURL});
+}
+
+// Search for a bug number in a string starting with: "bug ######"
+function getBugNumber(str) {
+  var parts = str.split(" ");
+  return (parts.length > 1 &&
+          parts[0].toLowerCase() == "bug" &&
+          parts[1].search(/^[0-9]{6}/) != -1)
+    ? parts[1].substr(0, 6) : null;
 }
